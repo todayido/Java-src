a089a923314b27a416ca4f0b552a8d70767bcdf6
momo <2227800977@qq.com>
2020-12-10 14:10:40 +0800

    Initial commit

---
+++ jdk.jdeps/com/sun/tools/jdeps/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.jdeps.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "14.0.2+12-46" },
+            { "jdk", "14.0.2" },
+            { "release", "14.0.2" },
+        };
+    }
+}
---
+++ jdk.jshell/jdk/internal/jshell/tool/resources/version.java
@@ -0,0 +1,11 @@
+package jdk.internal.jshell.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "14.0.2+12-46" },
+            { "jdk", "14.0.2" },
+            { "release", "14.0.2" },
+        };
+    }
+}
